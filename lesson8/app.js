bb27e8e90da3b0b77474cf9256b98f069350970a
crushedsummers <hairshinethesalonpony@gmail.com>
2019-03-02 15:38:07 +0800

    lesson8.1

--- lesson8/app.js
+++ lesson8/app.js
@@ -81,4 +81,28 @@ function update(){
 	game.physics.arcade.collide(player,platforms);
 	game.physics.arcade.collide(stars,platforms);
 	game.physics.arcade.collide(enemy,platforms);
+
+	//player not doing anything or default condition
+	player.body.velocity.x=0;
+
+	//player control
+	if (cursors.left.isDown){
+		player.body.velocity.x = (-150);
+		player.animations.play("left");
+	}
+	else if (cursors.right.isDown) {
+		player.body.velocity.x = 150;
+		player.animations.play("right");
+	}
+	else {
+		player.animations.stop();
+		player.frame = 4;
+	}
+	if (cursors.up.isDown && player.body.touching.down){
+		player.body.velocity.y = (-300);
+	}
+
+
+
+
 }
